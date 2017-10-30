9e114da5b069525ff5f94536d0956c68570a12fa
BJ Fink <bjfink23@gmail.com>
2017-10-30 00:15:21 -0400

    add link for router

--- src/components/TableOfContents.js
+++ src/components/TableOfContents.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import classnames from 'classnames';
+import { Link } from 'react-router-dom';
 
-function TableOfContents({ chapters, handleSelectChapter, selectedChapterId }) {
+function TableOfContents({ chapters, handleSelectChapter, selectedChapterId, baseUrl }) {
   return (
     <ul className="toc">
       {chapters.map(chapter => {
         const selected = chapter.id === selectedChapterId;
 
-        return (<li className={classnames({ 'active': selected })}
-          key={chapter.id}
-          onClick={() => handleSelectChapter(chapter.id)}>
-          {chapter.title}
-        </li>
+        return (
+          <Link to={`${chapter.id}`}  key={chapter.id} >
+            <li className={classnames({ 'active': selected })}>
+              {chapter.title}
+            </li>
+          </Link>
         )
       }
       )}
